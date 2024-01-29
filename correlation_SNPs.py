5121961ab02a0a1815d4c8e1b3e7d9d0a16a0519
Aje026 <60116113+Aje026@users.noreply.github.com>
2024-01-29 09:40:54 +0100

    correlation heatmap

--- correlation_SNPs.py
+++ correlation_SNPs.py
@@ -2,20 +2,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd. read_csv('all_pivoted.csv', index_col='rs_id')
-df = df.fillna(0)
-df = df.drop(columns='Unnamed: 0')
+df = pd.read_csv('genetic_correlation_psyck.csv', sep=';', index_col=0)
 
-correlation_matrix = df.corr(method='spearman')
+
 
 plt.figure(figsize=(15, 15))
 
 
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+sns.heatmap(df, annot=True, cmap='coolwarm', fmt=".2f")
 
 
 plt.title('Correlation Heatmap')
 
-
-plt.savefig('correlation_heatmap_4.jpg')
+plt.show()
+#plt.savefig('correlation_heatmap_4.jpg')
 #%%
