95836c91abc64b48cc6f74f1280de088816a6d6d
Aje026 <60116113+Aje026@users.noreply.github.com>
2024-03-07 14:09:28 +0100

    get rsID and extract tables from LDSC

---
+++ extract_table.py
@@ -0,0 +1,31 @@
+import os
+
+# Specify the directory where your log files are located
+log_files_directory = os.getcwd()  # Use the current working directory
+
+# Loop through log files from 2.log to 36.log
+for i in range(2, 37):
+    log_file_path = f'{i}.log'
+    
+    # Read the contents of the log file
+    with open(log_file_path, 'r') as file:
+        log_contents = file.readlines()
+
+    # Find the line number where the table starts
+    start_line = None
+
+    for j, line in enumerate(log_contents):
+        if line.startswith('Summary of Genetic Correlation Results'):
+            start_line = j 
+            break
+
+    # Extract the table lines
+    if start_line is not None:
+        table_lines = log_contents[start_line:]
+
+        # Save the extracted table to a new file
+        output_file_path = f'{i}_extracted_table.txt'
+        with open(output_file_path, 'w') as output_file:
+            output_file.writelines(table_lines)
+    else:
+        print(f"Table not found in {log_file_path}")
--- get_rsID.py
+++ get_rsID.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-build37 = pd.read_csv('build37.vcf',comment='#', sep='t')
-build38 = pd.read_csv('build38.vcf', comment='#', sep='\t')
+build37 = pd.read_csv('build37_clean.txt', sep='t')
+fibromyalgia=pd.read_csv('fibromyalgia_buildGRCh37.tsv', )
+#build38 = pd.read_csv('build38_clean.txt',sep='\t')
 
-endometriosis = pd.read_csv('endometriosis.txt', sep='\t')
+fibromyalgia=fibromyalgia.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
+fibromyalgia_updated = pd.merge(fibromyalgia, build37, on=['chr', 'pos'], how='inner')
+fibromyalgia_updated.to_csv('fibromyalgia_updated.txt', sep='\t')
+""" endometriosis = pd.read_csv('endometriosis.txt', sep='\t')
 diabetes1 = pd.read_csv('diabetes1.txt', sep='\t')
 fibromyalgia = pd.read_csv('fibromyalgia.txt', sep='\t')
-gdm = pd.read_csv('gdm.txt', sep='\t')
-pcos = pd.read_csv('pcos.txt', sep='\t')
-preeclampsia = pd.read_csv('preeclampsia.txt', sep='\t')
-preterm_birth = pd.read_csv('preterm_birth.txt', sep='\t')
-fatigue = pd.read_csv('fatigue.txt', sep='\t')
+gdm = pd.read_csv('gdm.txt', sep='\t') """
 
-#endometriosis=endometriosis.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
+
+
+""" preeclampsia = pd.read_csv('preeclampsia.txt', sep='\t')
+preterm_birth = pd.read_csv('preterm_birth.txt', sep='\t')
+fatigue = pd.read_csv('fatigue.txt', sep='\t') """
+
+""" #endometriosis=endometriosis.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 endometriosis_updated = pd.merge(endometriosis, build38, on=['chr', 'pos'], how='inner')
 endometriosis_updated.to_csv('endometriosis_updates.txt', sep='\t')
 
@@ -20,18 +26,15 @@ endometriosis_updated.to_csv('endometriosis_updates.txt', sep='\t')
 diabetes1_updated = pd.merge(diabetes1, build38, on=['chr', 'pos'], how='inner')
 diabetes1_updated.to_csv('diabetes_updated.txt', sep='\t')
 
-#fibromyalgia=fibromyalgia.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
-fibromyalgia_updated = pd.merge(fibromyalgia, build38, on=['chr', 'pos'], how='inner')
-fibromyalgia_updated.to_csv('fibromyalgia_updated.txt', sep='\t')
+
 
 #gdm=gdm.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 gdm_updated = pd.merge(gdm, build38, on=['chr', 'pos'], how='inner')
-gdm_updated.to_csv('gdm_updated.txt', sep='\t')
+gdm_updated.to_csv('gdm_updated.txt', sep='\t') """
 
-pcos_updated = pd.merge(pcos, build37, on=['chr', 'pos'], how='inner')
-pcos_updated.to_csv('pcos_updated.txt', sep='\t')
 
-#preeclampsia=preeclampsia.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
+
+""" #preeclampsia=preeclampsia.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 preeclampsia_updated = pd.merge(preeclampsia, build38, on=['chr', 'pos'], how='inner')
 preeclampsia_updated.to_csv('preeclampsia_updated.txt', sep='\t')
 
@@ -41,5 +44,9 @@ preterm_birth_updated.to_csv('preterm_birth_updated.txt', sep='\t')
 
 #fatigue=preterm_birth.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 fatigue_updated = pd.merge(fatigue, build38, on=['chr', 'pos'], how='inner')
-fatigue_updated.to_csv('fatigue_updated.txt', sep='\t')
+fatigue_updated.to_csv('fatigue_updated.txt', sep='\t') """
 
+""" 
+pcos = pd.read_csv('pcos.txt', sep='\t')
+pcos_updated = pd.merge(pcos, build37, on=['chr', 'pos'], how='inner')
+pcos_updated.to_csv('pcos_updated.txt', sep='\t') """
