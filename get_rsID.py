7b8028e67f1841234b7bb0505635b9b4988f4a09
Aje026 <60116113+Aje026@users.noreply.github.com>
2024-03-13 11:14:36 +0100

    small fix analysis

--- get_rsID.py
+++ get_rsID.py
@@ -1,28 +1,28 @@
 import pandas as pd
 
 
-#build37 = pd.read_csv('build37.vcf', sep='\t', header=55)
+build37 = pd.read_csv('build37.vcf', sep='\t', header=55)
 
 build38 = pd.read_csv('build38.vcf',sep='\t', header=55)
 
 
-#build37 = build37[['#CHROM', 'POS', 'ID']]
+build37 = build37[['#CHROM', 'POS', 'ID']]
 build38 = build38[['#CHROM', 'POS', 'ID']] 
 
-#build37 = build37.rename(columns={'#CHROM':'chr', 'POS':'pos'})
+build37 = build37.rename(columns={'#CHROM':'chr', 'POS':'pos'})
 build38 = build38.rename(columns={'#CHROM':'chr', 'POS':'pos'})
 
-""" fibromyalgia=pd.read_csv('fibromyalgia_buildGRCh37.tsv', sep='\t')
+""" fibromyalgia=pd.read_csv('fibromyalgia_buildGRCh37.tsv',sep='\t')
 fibromyalgia=fibromyalgia.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 fibromyalgia_updated = pd.merge(fibromyalgia, build37, on=['chr', 'pos'], how='inner')
-fibromyalgia_updated.to_csv('fibromyalgia_updated2.tsv', sep='\t', index=False) """
+fibromyalgia_updated.to_csv('fibromyalgia_updated3.tsv', sep='\t', index=False) """
 
 
 
-""" endometriosis = pd.read_csv('endometriosis_buildgrch37.txt', sep='\t')
+endometriosis = pd.read_csv('endometriosis_buildgrch37.txt', sep='\t')
 endometriosis=endometriosis.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 endometriosis_updated = pd.merge(endometriosis, build37, on=['chr', 'pos'], how='inner')
-endometriosis_updated.to_csv('endometriosis_updates2.tsv', sep='\t', index=False) """
+endometriosis_updated.to_csv('endometriosis_updated3.tsv', index=False, sep='\t')
 
 
 
@@ -32,16 +32,16 @@ endometriosis_updated.to_csv('endometriosis_updates2.tsv', sep='\t', index=False
 preeclampsia = pd.read_csv('preeclampsia.tsv', sep='\t')
 preeclampsia=preeclampsia.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 preeclampsia_updated = pd.merge(preeclampsia, build38, on=['chr', 'pos'], how='inner')
-preeclampsia_updated.to_csv('preeclampsia_updated2.tsv', sep='\t', index=False)
+preeclampsia_updated.to_csv('preeclampsia_updated3.tsv',index=False, sep='\t')
 
 
 preterm_birth = pd.read_csv('spontanious_preterm_birth.tsv', sep='\t')
 preterm_birth=preterm_birth.rename(columns={'chromosome':'chr','base_pair_location':'pos'})
 preterm_birth_updated = pd.merge(preterm_birth, build38, on=['chr', 'pos'], how='inner')
-preterm_birth_updated.to_csv('preterm_birth_updated2.tsv', sep='\t', index=False)
+preterm_birth_updated.to_csv('preterm_birth_updated3.tsv', index=False, sep='\t')
 
 
 
-""" pcos = pd.read_csv('pcos.txt', sep='\t')
+pcos = pd.read_csv('pcos.txt', sep='\t')
 pcos_updated = pd.merge(pcos, build37, on=['chr', 'pos'], how='inner')
-pcos_updated.to_csv('pcos_updated2.tsv', sep='\t', index=False)  """
+pcos_updated.to_csv('pcos_updated3.tsv', index=False, sep='\t') 
